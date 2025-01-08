9a75099b54b50faa007f341b3374c4047831dd9d
DevShineWunnaAung <67731642+DevShineWunnaAung@users.noreply.github.com>
2025-01-08 18:42:26 +0700

    Add Js

---
+++ match.js
@@ -0,0 +1,73 @@
+var match_data = [
+    {
+      "id": "01",
+      "league": "Spanish Super Cup",
+      "time": "01:30 AM",
+      "date": "Jan 09",
+      "home_name": "Athletic Bilbao",
+      "home_img": "https://mygoaltv.xyz/team/athletic-bilbao.png",
+      "away_name": "Barcelona",
+      "away_img": "https://mygoaltv.xyz/team/Barcelona.png"
+    },
+    {
+      "id": "02",
+      "league": "Portugal Cup",
+      "time": "02:15 AM",
+      "date": "Jan 09",
+      "home_name": "Benfica",
+      "home_img": "https://mygoaltv.xyz/team/Benfica.png",
+      "away_name": "Sporting Braga",
+      "away_img": "https://mygoaltv.xyz/team/Sporting-Braga.png"
+    },
+    {
+      "id": "03",
+      "league": "EFL Cup",
+      "time": "02:30 AM",
+      "date": "Jan 09",
+      "home_name": "Tottenham",
+      "home_img": "https://mygoaltv.xyz/team/tottenham.png",
+      "away_name": "Liverpool",
+      "away_img": "https://mygoaltv.xyz/team/liverpool.png"
+    },
+    {
+      "id": "04",
+      "league": "Saudi Pro League",
+      "time": "11:30 PM",
+      "date": "Jan 09",
+      "home_name": "Al-Nassr",
+      "home_img": "https://mygoaltv.xyz/team/al-nassr.png",
+      "away_name": "Al Akhdoud",
+      "away_img": "https://mygoaltv.xyz/team/no-logo.png"
+    },
+    {
+      "id": "05",
+      "league": "Spanish Super Cup",
+      "time": "01:30 AM",
+      "date": "Jan 10",
+      "home_name": "Real Madrid",
+      "home_img": "https://mygoaltv.xyz/team/real-madrid.png",
+      "away_name": "Mallorca",
+      "away_img": "https://mygoaltv.xyz/team/Mallorca.gif"
+    },
+    {
+      "id": "06",
+      "league": "FA Cup",
+      "time": "02:15 AM",
+      "date": "Jan 10",
+      "home_name": "Fulham",
+      "home_img": "https://mygoaltv.xyz/team/fulham.png",
+      "away_name": "Watford",
+      "away_img": "https://mygoaltv.xyz/team/Watford.gif"
+    },
+    {
+      "id": "07",
+      "league": "FA Cup",
+      "time": "02:15 AM",
+      "date": "Jan 10",
+      "home_name": "Everton",
+      "home_img": "https://mygoaltv.xyz/team/everton.png",
+      "away_name": "Peterborough United",
+      "away_img": "https://mygoaltv.xyz/team/Peterborough_United.png"
+    }
+  ];
+  
